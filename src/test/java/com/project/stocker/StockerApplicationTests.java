b0bfdd3e6394ac553f2ab90f1716a2c95a58607f
downside154 <75163801+downside154@users.noreply.github.com>
2023-08-05 12:25:38 +0900

    first commit

---
+++ src/test/java/com/project/stocker/StockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
