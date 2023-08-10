4d7ce2f6d1a86b1cc807785f07fac7548f38360e
uijin <powerlife145@gmail.com>
2023-08-10 16:46:52 +0900

    feat/Trading volume, increase in width, decrease in width

--- src/main/java/com/project/stocker/ranking/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.stocker.ranking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface StockRepository extends JpaRepository<Stock, Long> {
-    List<Stock> findAll();
-}
