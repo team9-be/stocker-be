65e44a62bd307023478c2edac8c11fb847ab9f71
downside154 <75163801+downside154@users.noreply.github.com>
2023-08-31 14:26:04 +0900

    feat: db lock and spellcheck

--- src/main/java/com/project/stocker/repository/TradeRepository.java
+++ src/main/java/com/project/stocker/repository/TradeRepository.java
@@ -1,19 +1,37 @@
 package com.project.stocker.repository;
 
 import com.project.stocker.entity.Trade;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
 
-    List<Trade> findByStock_IdAndCreatedAtBeforeAndCreatedAtAfter(Long stockId, LocalDateTime endTime, LocalDateTime startTime);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Trade> findByStock_IdAndCreatedAtBeforeAndCreatedAtAfter(
+            Long stockId,
+            LocalDateTime endTime,
+            LocalDateTime startTime
+    );
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Trade findTop1ByStock_IdAndBuyerIsNotNullAndSellerIsNotNullOrderByCreatedAtDesc(Long stockId);
 
-    Trade findTop1ByStock_IdAndCreatedAtBeforeAndCreatedAtAfterOrderByCreatedAtDesc(Long stockId, LocalDateTime yesterdayLast, LocalDateTime yesterdayFirst);
 
-    Trade findTop1ByStock_IdAndCreatedAtBeforeAndCreatedAtAfterOrderByCreatedAtAsc(Long stockId, LocalDateTime localDateTime, LocalDateTime localDateTime1);
+    Trade findTop1ByStock_IdAndCreatedAtBeforeAndCreatedAtAfterOrderByCreatedAtDesc(
+            Long stockId,
+            LocalDateTime yesterdayLast,
+            LocalDateTime yesterdayFirst
+    );
+
+
+    Trade findTop1ByStock_IdAndCreatedAtBeforeAndCreatedAtAfterOrderByCreatedAtAsc(
+            Long stockId,
+            LocalDateTime localDateTime,
+            LocalDateTime localDateTime1
+    );
 
 }
