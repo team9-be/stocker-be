12e9a316fac9bb62c20c29d43df96bd0b8d0afc2
downside154 <75163801+downside154@users.noreply.github.com>
2023-09-02 01:37:12 +0900

    feat: remove pessimistic lock

--- src/main/java/com/project/stocker/repository/TradeRepository.java
+++ src/main/java/com/project/stocker/repository/TradeRepository.java
@@ -1,23 +1,19 @@
 package com.project.stocker.repository;
 
 import com.project.stocker.entity.Trade;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Trade> findByStock_IdAndCreatedAtBeforeAndCreatedAtAfter(
             Long stockId,
             LocalDateTime endTime,
             LocalDateTime startTime
     );
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Trade findTop1ByStock_IdAndBuyerIsNotNullAndSellerIsNotNullOrderByCreatedAtDesc(Long stockId);
 
 
