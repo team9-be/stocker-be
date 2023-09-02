d8d3c4bd7ce5dab4c5c8f55419b3b617e9282a0a
ByHorizon <rptedpa5@naver.com>
2023-09-02 14:27:10 +0900

    feat: sellAccount

--- src/main/java/com/project/stocker/controller/AccountController.java
+++ src/main/java/com/project/stocker/controller/AccountController.java
@@ -1,12 +1,11 @@
 package com.project.stocker.controller;
 
+import com.project.stocker.dto.request.AccountAddStockRequest;
 import com.project.stocker.dto.response.AccountResponseDto;
 import com.project.stocker.service.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +17,10 @@ public class AccountController {
     @GetMapping("/user/{userId}/account/get")
     public List<AccountResponseDto> getMyAccount(@PathVariable Long userId){
         return accountService.getMyAccount(userId);
+    }
+    @PutMapping("/user/{userId}/account/addStock")
+    public ResponseEntity<String> addStock(@PathVariable Long userId, @RequestBody AccountAddStockRequest requestDto){
+        accountService.addStock(userId, requestDto);
+        return ResponseEntity.ok("성공적으로 주식을 추가했습니다.");
     }
 }
--- src/main/java/com/project/stocker/service/AccountService.java
+++ src/main/java/com/project/stocker/service/AccountService.java
@@ -1,14 +1,12 @@
 package com.project.stocker.service;
 
+import com.project.stocker.dto.request.AccountAddStockRequest;
 import com.project.stocker.dto.response.AccountResponseDto;
 import com.project.stocker.entity.Account;
-import com.project.stocker.entity.Trade;
-import com.project.stocker.entity.User;
 import com.project.stocker.repository.AccountRepository;
 import com.project.stocker.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,8 +14,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
 
     public List<AccountResponseDto> getMyAccount(Long userId) {
         return accountRepository.findAllByUserId(userId).stream().map(AccountResponseDto::new).toList();
+    }
+
+    public void addStock(Long userId, AccountAddStockRequest requestDto) {
+        if(accountRepository.findByUserIdAndStockCompany(userId, requestDto.getStockCompany()).isEmpty()){
+            Account account = new Account(userRepository.findById(userId).get(), requestDto);
+            accountRepository.save(account);
+        }
+        else {
+            Account account = accountRepository.findByUserIdAndStockCompany(userId,
+                    requestDto.getStockCompany()).get();
+            account.changeQuantity(requestDto.getQuantity());
+        }
     }
 }
