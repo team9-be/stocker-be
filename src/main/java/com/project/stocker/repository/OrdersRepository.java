0a1da97e7b928bb65f0456e9febe95487c6d88d7
CodingDoor <powerlife145@gmail.com>
2023-09-07 21:00:58 +0900

    edit: matchOrder (#68)
    * merge: develop
    
    * merge: tradeservice
    
    * feat: thread pool
    
    * feat: spring thread pool
    
    * edit: matchOrder edit algorithm

--- src/main/java/com/project/stocker/repository/OrdersRepository.java
+++ src/main/java/com/project/stocker/repository/OrdersRepository.java
@@ -3,5 +3,10 @@ package com.project.stocker.repository;
 import com.project.stocker.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    List<Orders> findAllByBuyerIsNotNullAndSellerIsNull();
+
+    List<Orders> findAllBySellerIsNotNullAndBuyerIsNull();
 }
